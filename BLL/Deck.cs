b9ec5423081970f307b5b0e86d0678e1c962b356
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-08-01 20:12:13 -0300

    feat(ajustes operações com deck)

--- BLL/Deck.cs
+++ BLL/Deck.cs
@@ -1,18 +1,26 @@
 ﻿using Godot;
+using System;
+using System.Linq;
 using System.Collections.Generic;
+
+using BLL.Utils;
 
 namespace BLL
 {
     public class Deck
     {
         public List<DTO.Card> PlanarDeck { get; set; }
+        public List<DTO.Card> Cemitery { get; set; }
 
         public Deck()
         {
             PlanarDeck = new List<DTO.Card>();
+            Cemitery = new List<DTO.Card>();
         }
         private void LoadDeck()
         {
+            PlanarDeck.Clear();
+            Cemitery.Clear();
             PlanarDeck = LoadCards(CardList.Plane, DTO.Type.Plane);
             if (Options.EnablePhenomenons)
                 PlanarDeck.AddRange(LoadCards(CardList.Phenomenon, DTO.Type.Plane));
@@ -27,11 +35,34 @@ namespace BLL
                 {
                     CardType = cardType,
                     FilePath = card,
-                    //CardImage = 
+                    CardImage = ImportadorDeImagensUtil.BuscarImagem(string.Empty, card) 
                 });
             }
 
             return deck;
+        }
+        public DTO.Card RevealTopCard()
+        {
+            try
+            {
+                return PlanarDeck.FirstOrDefault();
+            }
+            catch
+            {
+                LoadDeck();
+                ShuffleDeck();
+                return PlanarDeck.FirstOrDefault();
+            }
+        }
+        public void SendToCemitery(DTO.Card card)
+        {
+            PlanarDeck.Remove(card);
+            Cemitery.Add(card);
+        }
+        public void ShuffleDeck()
+        {
+            var random = new Random();
+            PlanarDeck = PlanarDeck.OrderBy(item => random.Next()).ToList();
         }
     }
 }
---
+++ BLL/Utils/ImportadorDeImagensUtil.cs
@@ -0,0 +1,30 @@
+using System;
+using Godot;
+
+namespace BLL.Utils
+{
+    public static class ImportadorDeImagensUtil
+    {
+        public static long Counter { get; set; } = 0;
+        public static Texture BuscarImagem(string nomeImagem, string caminho)
+        {
+            var imagem = new Image();
+            var texturaDaImagem = new ImageTexture();
+            var caminhoComFormato = caminho + nomeImagem + ".jpg";
+            var caminhoImport = caminhoComFormato + ".import";
+
+            imagem.Load(caminhoComFormato);
+            texturaDaImagem.CreateFromImage(imagem);
+            LimparArquivosTemporarios(caminhoComFormato, caminhoImport);
+            
+            return texturaDaImagem;
+        }
+        private static void LimparArquivosTemporarios(string caminhoComFormato, string caminhoImport)
+        {
+            if (System.IO.File.Exists(caminhoComFormato))
+                System.IO.File.Delete(caminhoComFormato);
+            if (System.IO.File.Exists(caminhoImport))
+                System.IO.File.Delete(caminhoImport);
+        }
+    }
+}
