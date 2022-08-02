248c0e7e75bcbdf4d627f0a09be42e4ef566507f
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-08-02 09:27:07 -0300

    feat(ajustes menu de opções)

--- CTRL/MainCTRL.cs
+++ CTRL/MainCTRL.cs
@@ -1,96 +1,106 @@
 using Godot;
 using System;
 
-public class MainCTRL : Control
+namespace CTRL
 {
-	private bool FirstCard { get; set; }
-	private TextureButton Card { get; set; }
-	private TextureButton Phenomenon { get; set; }
-	private BLL.Interface.IDeck PlanarDeck { get; set; }
-	private DTO.Card ActualPlane { get; set; }
-	private DTO.Card ActualPhenomenon { get; set; }
-	public override void _Ready()
+	public class MainCTRL : Control
 	{
-		PopularNodes();
-		RealizarInjecaoDeDependencias();
-		DesativarFuncoesDeAltoProcessamento();
-	}
-	private void PopularNodes()
-	{
-		Card = GetNode<TextureButton>("./Card");
-		Phenomenon = GetNode<TextureButton>("./ConteinerHorizontal/Comandos/Fenomeno");
-	}
-	private void RealizarInjecaoDeDependencias()
-	{
-		FirstCard = true;
-		ActualPlane = null;
-		ActualPhenomenon = null;
-		PlanarDeck = new BLL.Deck(Card.TextureNormal);
-		PlanarDeck.LoadDeck();
-		PlanarDeck.ShuffleDeck();
-		Phenomenon.Visible = false;
-	}
-	private void DesativarFuncoesDeAltoProcessamento()
-	{
-		SetProcess(false);
-		SetPhysicsProcess(false);
-	}
-	private void RevealCard()
-	{
-		var card = PlanarDeck.RevealTopCard();
-		if (card.CardType == DTO.Type.Plane)
+		private bool FirstCard { get; set; }
+		private TextureButton Card { get; set; }
+		private TextureButton Phenomenon { get; set; }
+		private BLL.Interface.IDeck PlanarDeck { get; set; }
+		private DTO.Card ActualPlane { get; set; }
+		private DTO.Card ActualPhenomenon { get; set; }
+		private Control OptionMenu { get; set; }
+		public override void _Ready()
 		{
-			ActualPlane = card;
+			PopularNodes();
+			RealizarInjecaoDeDependencias();
+			DesativarFuncoesDeAltoProcessamento();
+			AlterarVisibilidadeMenu(false);
+		}
+		private void PopularNodes()
+		{
+			Card = GetNode<TextureButton>("./Card");
+			Phenomenon = GetNode<TextureButton>("./ConteinerHorizontal/Comandos/Fenomeno");
+			OptionMenu = GetNode<Control>("./MenuDeOpcoes");
+		}
+		private void RealizarInjecaoDeDependencias()
+		{
+			FirstCard = true;
+			ActualPlane = null;
 			ActualPhenomenon = null;
-			Card.TextureNormal = ActualPlane.CardImage;
+			PlanarDeck = new BLL.Deck(Card.TextureNormal);
+			PlanarDeck.LoadDeck();
+			PlanarDeck.ShuffleDeck();
 			Phenomenon.Visible = false;
 		}
-		else
+		private void DesativarFuncoesDeAltoProcessamento()
 		{
-			ActualPhenomenon = card;
-			Card.TextureNormal = ActualPhenomenon.CardImage;
-			while(card.CardType != DTO.Type.Plane)
+			SetProcess(false);
+			SetPhysicsProcess(false);
+		}
+		private void AlterarVisibilidadeMenu(bool visivel)
+		{
+			OptionMenu.RectPosition = visivel ? new Vector2(0,0) : new Vector2(5000,0);
+		}
+		private void RevealCard()
+		{
+			var card = PlanarDeck.RevealTopCard();
+			if (card.CardType == DTO.Type.Plane)
 			{
-				PlanarDeck.SendToCemitery(card);
-				card = PlanarDeck.RevealTopCard();
+				ActualPlane = card;
+				ActualPhenomenon = null;
+				Card.TextureNormal = ActualPlane.CardImage;
+				Phenomenon.Visible = false;
 			}
-			ActualPlane = card;
-			Phenomenon.Visible = true;
+			else
+			{
+				ActualPhenomenon = card;
+				Card.TextureNormal = ActualPhenomenon.CardImage;
+				while(card.CardType != DTO.Type.Plane)
+				{
+					PlanarDeck.SendToCemitery(card);
+					card = PlanarDeck.RevealTopCard();
+				}
+				ActualPlane = card;
+				Phenomenon.Visible = true;
+			}
 		}
-	}
-	private void _on_Card_button_up()
-	{
-		if (FirstCard)
+		private void _on_Card_button_up()
 		{
-			RevealCard();
-			FirstCard = false;
+			if (FirstCard)
+			{
+				RevealCard();
+				FirstCard = false;
+			}
+			if (Phenomenon.Visible)
+			{
+				Card.TextureNormal = ActualPlane.CardImage;
+			}
 		}
-		if (Phenomenon.Visible)
+		private void _on_Transplanar_button_up()
 		{
-			Card.TextureNormal = ActualPlane.CardImage;
+			if (!FirstCard)
+			{
+				PlanarDeck.SendToCemitery(ActualPlane);
+				RevealCard();
+			}
 		}
-	}
-	private void _on_Transplanar_button_up()
-	{
-		if (!FirstCard)
+		private void _on_Ajustes_button_up()
 		{
-			PlanarDeck.SendToCemitery(ActualPlane);
-			RevealCard();
+			AlterarVisibilidadeMenu(true);
 		}
-	}
-	private void _on_Ajustes_button_up()
-	{
-		// Replace with function body.
-	}
-	private void _on_Embaralhar_button_up()
-	{
-		Card.TextureNormal = PlanarDeck.Verso;
-		FirstCard = true;
-		PlanarDeck.ShuffleDeck();
-	}
-	private void _on_Fenomeno_button_up()
-	{
-		if (ActualPhenomenon != null)
-			Card.TextureNormal = ActualPhenomenon.CardImage;
+		private void _on_Embaralhar_button_up()
+		{
+			Card.TextureNormal = PlanarDeck.Verso;
+			FirstCard = true;
+			PlanarDeck.ShuffleDeck();
+		}
+		private void _on_Fenomeno_button_up()
+		{
+			if (ActualPhenomenon != null)
+				Card.TextureNormal = ActualPhenomenon.CardImage;
+		}
 	}
 }
