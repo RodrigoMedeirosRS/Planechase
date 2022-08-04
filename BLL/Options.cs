3c315979f724de20222836981903cba05006a0d7
RodrigoMedeirosRS <rodrigo.medeiros.email@gmail.com>
2022-08-04 08:26:18 -0300

    feat(ajustes publicação)

--- BLL/Options.cs
+++ BLL/Options.cs
@@ -3,7 +3,7 @@ namespace BLL
     public static class Options
     {
         public static bool EnablePhenomenons { get; set; } = true;
-        public static bool EnableCustomCards { get; set; } = true;
+        public static bool EnableCustomCards { get; set; } = false;
         public static int ChaosFaces { get; set; } = 1;
         public static int TransplaneFaces { get; set; } = 1;
     }
